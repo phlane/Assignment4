6e3c5b168b966f3353574375ef9eb68e3c7f836b
Charles Winkelman <charles@chw.ink>
2024-02-21 15:27:10 -0500

    Initial commit

---
+++ src/accidentpack/program4.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package accidentpack;
+
+/**
+ * 
+ */
+public class program4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
